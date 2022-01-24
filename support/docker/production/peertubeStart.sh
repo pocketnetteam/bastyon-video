67effcf29f30f4c20df17f6219dd8ae48a63880b
Alexander <alavrukhine@rixtrema.com>
2022-01-24 13:54:27 +0300

    updated installation scripts

---
+++ support/docker/production/peertubeStart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+npm run start
+#First start options
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First container startup --"
+    # Change Admin Password
+    echo $PEERTUBE_ROOT_PASSWORD | NODE_CONFIG_DIR=/app/config NODE_ENV=production npm run reset-password -- -u root;
+
+    # Install Plugins
+    NODE_CONFIG_DIR=/app/config NODE_ENV=production npm run plugin:install -- --npm-name peertube-plugin-pocketnet-auth;
+else
+    echo "-- Not first container startup --"
+fi
