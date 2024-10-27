54e59cc0aa3019a583431365906a80219f702a0d
Alexander <zanhesl@gmail.com>
2024-10-27 14:15:23 +0300

    Hide video watch

--- server/initializers/config.ts
+++ server/initializers/config.ts
@@ -181,7 +181,7 @@ const CONFIG = {
       CHECK_INTERVAL: parseDurationToMs(config.get<string>('redundancy.videos.check_interval')),
       STRATEGIES: buildVideosRedundancy(config.get<any[]>('redundancy.videos.strategies'))
     },
-    GARBAGE_COLLECTOR_CHECK_INTERVAL: parseDurationToMs(config.get<string>('redundancy.garbage_collector_check_interval')),
+    GARBAGE_COLLECTOR_CHECK_INTERVAL: parseDurationToMs(config.get<string>('redundancy.garbage_collector_check_interval'))
   },
   REMOTE_REDUNDANCY: {
     VIDEOS: {
